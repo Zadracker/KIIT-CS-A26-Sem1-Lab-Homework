9874dd1f9156773b4ba93481e357fa8ef793efe8
Zayyan Dalai <90850469+Zadracker@users.noreply.github.com>
2024-11-13 16:14:45 +0530

    added output class_13 - q2

--- Class_13/q2.c
+++ Class_13/q2.c
@@ -35,3 +35,37 @@ for (int i = 0; i < 5; i++) {
     printf("%d\t%s\t%d\n", emp[i].id, emp[i].name, emp[i].age);
 }
 return 0;}
+
+//Output
+/*
+Enter Employee 1 Name: Kabir
+Enter Employee 1 ID: 1
+Enter Employee 1 Age: 18
+
+Enter Employee 2 Name: Aryan
+Enter Employee 2 ID: 2
+Enter Employee 2 Age: 20
+
+Enter Employee 3 Name: Sonia
+Enter Employee 3 ID: 3
+Enter Employee 3 Age: 19
+
+Enter Employee 4 Name: Steve
+Enter Employee 4 ID: 4
+Enter Employee 4 Age: 30
+
+Enter Employee 5 Name: Kim
+Enter Employee 5 ID: 5
+Enter Employee 5 Age: 24
+
+
+Employee Information:
+
+ID	Name	Age
+--------------------
+1	Kabir	18
+2	Aryan	20
+3	Sonia	19
+4	Steve	30
+5	Kim     24
+*/
